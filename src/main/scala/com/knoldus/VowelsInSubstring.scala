467b15930debf3625e7bc4cf6544c88fde227f6c
umang-goyal <goel97umang@gmail.com>
2020-02-03 16:33:57 +0530

    added optimised code for finding vowels in sub string

--- src/main/scala/com/knoldus/VowelsInSubstring.scala
+++ src/main/scala/com/knoldus/VowelsInSubstring.scala
@@ -1,42 +1,32 @@
 package com.knoldus
 
-class SubstringWithVowels {
-  def count (string: String): Int ={
-    val l:Int = string.length
-
-    val arr: Array[Int] = new Array[Int](78)
-    @scala.annotation.tailrec
-    def numberOfSubstrings(string: String, i:Int, array: Array[Int],l: Int): Array[Int] ={
-      if (i==0){
-        array(i) = l
-      }
-      else if (i== l-1){
-        return array
-      }
-      else {
-        array(i)= array(i-1) + (l - i) - i
-      }
-      numberOfSubstrings(string, i+1, array,l)
+class VowelsInSubstring {
+  def count (string: String): Int = {
+    def countSub(l: Int, index: Int): Int = {
+      (index+1)*(l-index)
     }
-    numberOfSubstrings(string, 0, arr, l)
 
-    def vowelsCheck(array: Array[Int], string: String, i: Int, sumArray: Array[Int]): Int ={
-      if (i == string.length) {
+    val sumArray = new Array[Int](1)
+
+    def iterChar(string: String, index:Int, array: Array[Int]):Int={
+      if (index == string.length){
         return sumArray(0)
       }
-      if (string(i) =='a' ||string(i)=='e' ||string(i)=='i' ||string(i)=='o' ||string(i)=='u'){
-        sumArray(0)+= array(i)
-
+      if (string(index)=='a'||string(index)=='e'||string(index)=='i'||string(index)=='o'||string(index)=='u'){
+        //println(countSub(string.length, index))
+        sumArray(0) += countSub(string.length, index)
       }
-      vowelsCheck(arr, string,i+1, sumArray)
+      iterChar(string, index+1, sumArray)
       sumArray(0)
     }
-    val sumArray = new Array[Int](1)
-    vowelsCheck(arr, string, 0, sumArray)
+
+    iterChar(string,0,sumArray)
   }
+
 }
 
-object SubstringInVowels extends App{
-  val obj = new SubstringWithVowels
-  print(obj.count("baceb"))
+object VowelsInSubstring extends App{
+  val obj = new VowelsInSubstring
+  println(obj.count("aaa"))
+  println(obj.count("baceb"))
 }
