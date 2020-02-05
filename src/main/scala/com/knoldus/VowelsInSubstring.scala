a7aeb3a01c81108c606048e0571ac3c432067051
umang-goyal <goel97umang@gmail.com>
2020-02-05 12:21:09 +0530

    improved code by implementing tail recursion and removing mutable data type.

--- src/main/scala/com/knoldus/VowelsInSubstring.scala
+++ src/main/scala/com/knoldus/VowelsInSubstring.scala
@@ -1,27 +1,26 @@
 package com.knoldus
 
 class VowelsInSubstring {
-  def count (string: String): Int = {
-    def countSub(l: Int, index: Int): Int = {
-      (index+1)*(l-index)
-    }
-
-    val sumArray = new Array[Int](1)
-
-    def iterChar(string: String, index:Int, array: Array[Int]):Int={
-      if (index == string.length){
-        return sumArray(0)
-      }
-      if (string(index)=='a'||string(index)=='e'||string(index)=='i'||string(index)=='o'||string(index)=='u'){
-        //println(countSub(string.length, index))
-        sumArray(0) += countSub(string.length, index)
-      }
-      iterChar(string, index+1, sumArray)
-      sumArray(0)
-    }
-
-    iterChar(string,0,sumArray)
+def count (string: String): Int = {
+  def countSub(l: Int, index: Int): Int = {
+    (index+1)*(l-index)
   }
+
+  @scala.annotation.tailrec
+  def iterChar(string: String, index:Int, sum:Int):Int={
+    if (index == string.length){
+      return sum
+    }
+    else if (string(index)=='a'||string(index)=='e'||string(index)=='i'||string(index)=='o'||string(index)=='u'){
+      iterChar(string, index+1, sum + countSub(string.length, index))
+    }
+    else {
+      iterChar(string, index + 1, sum)
+    }
+  }
+
+  iterChar(string,0,0)
+}
 
 }
 
